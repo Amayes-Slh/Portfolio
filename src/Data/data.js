11f438d485c7ff46442f7cb914ac3f795352c032
Amayes-ExR <amayes.salhi@exxact-robotics.com>
2022-11-13 16:17:44 +0100

    Work projects added

---
+++ src/Data/data.js
@@ -0,0 +1,38 @@
+import robotics from '../assets/projects/robotics.png'
+import object_detection from '../assets/projects/object_detection.png'
+import smart_watch from '../assets/projects/smart_watch.png'
+import website from '../assets/projects/website.png'
+
+export const data=[
+ 
+   {
+        id:1,
+        image:smart_watch,
+        github:"",
+        live:"",
+    },
+
+    {
+        id:2,
+        image:object_detection,
+        github:"",
+        live:"",
+    },
+
+    {
+        id:3,
+        image:robotics,
+        github:"",
+        live:"",
+    },
+
+    {
+        id:4,
+        image:website,
+        github:"",
+        live:"",
+    },
+
+
+
+]
